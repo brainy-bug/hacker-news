c389d61cb62827f1b041049df8cc0b79f20dc162
maodesigns <mao.devv@gmail.com>
2023-02-25 02:21:09 +0100

    Added styles to scroll btn

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <SearchForm />
       <Buttons />
       <Stories />
-      <ScrollToTop smooth={true} className='scrollToTop' />
+      <ScrollToTop smooth={true} className='scrollToTop' height='22'  />
     </>
   );
 }
